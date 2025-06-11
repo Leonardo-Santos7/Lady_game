5a0d4290e9e1da180db425892debebe6e01f13be
Luigi Mateus <87377118+luigiikk@users.noreply.github.com>
2025-06-11 17:24:25 -0300

    ✨feat: add core with logic interface and logic implements

---
+++ src/implementation/core/CheckerLogicImp.java
@@ -0,0 +1,77 @@
+package implementation.core;
+
+import implementation.model.*;
+
+public class CheckerLogicImp implements checkerLogic{
+    private Board board;
+    private Player player1;
+    private Player player2;
+    private Player playerActual;
+
+    public CheckerLogicImp(){
+        startGame();
+    }
+
+    private void startGame(){
+        board = new Board();
+        player1 = new Player(1, "luigi");
+        player2 = new Player(2, "Leonardo");
+        playerActual = player1;
+
+        board.start();
+    }
+
+    @Override
+    public boolean containsPeace(PositionCheckers positionPeace) {
+        Peace peace = board.getPeace(positionPeace);
+
+        if(peace == null){
+            return false;
+        }
+
+        return peace.getPlayer() == playerActual.getNumber();
+    }
+
+    @Override
+    public boolean validateMovement(PositionCheckers positionCheckers) {
+        return true;
+    }
+
+    @Override
+    public void executeMovement(PositionCheckers positionDestination, PositionCheckers positionOrigin) {
+        boolean isCaputure = positionOrigin.rowDistance(positionDestination) == 2;
+        PositionCheckers capturedPosition = null;
+
+        if(isCaputure){
+            int row = (positionOrigin.getRow() + positionDestination.getRow()) / 2;
+            int col = (positionOrigin.getColumn() + positionDestination.getColumn()) / 2;
+            capturedPosition = new PositionCheckers(row, col);
+        }
+
+        Movement movement = new Movement(positionOrigin, positionDestination, isCaputure, capturedPosition);
+
+        board.movementPeace(movement);
+    }
+
+    @Override
+    public void restartGame() {
+        startGame();
+    }
+
+
+
+    @Override
+    public int getPlayerActual() {
+        return playerActual.getNumber()     ;
+    }
+
+    @Override
+    public int getTypePeace(PositionCheckers positionCheckers) {
+        Peace peace =  board.getPeace(positionCheckers);
+
+        Peace.Type type = peace.getType();
+
+        return type.getValue();
+
+    }
+}
